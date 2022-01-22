62335aea12932533fb9f912b42c008cbee538c77
unknown <you@example.com>
2022-01-22 16:46:41 +0530

    changed db import and added wsgi server

--- spa-backend/app.py
+++ spa-backend/app.py
@@ -1,6 +1,7 @@
 from logging import debug
 from flask import Flask, app, jsonify, request
 from flask_cors import CORS
+from db import db
 
 from resources.Rsparaw import *
 from resources.Rspafinal import *
@@ -13,6 +14,8 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SPdata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'key'
+
+db.init_app(app)
 
 
 @app.before_first_request
@@ -30,6 +33,6 @@ app.register_blueprint(FinalProduct)
 app.register_blueprint(ProcessProduct)
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(host='0.0.0.0', debug=True)
+    #from db import db
+    # db.init_app(app)
+    app.run(host='0.0.0.0', debug=Frue)
